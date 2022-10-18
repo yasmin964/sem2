2eb993758624e31d47a3d1e22a2c1a8bf9e7df66
yasya <kalgexavva1970@icloud.com>
2022-10-18 21:16:05 +0300

    add new text

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите программу,которая принимает на вход число и выводит кратно ли оно на 7 и 23 одновременно
+Console.Write("введите число: ");
+int num=int.Parse(Console.Readline()!);
+if (num/7 && num/23)
+Console.WriteLine($"{num}-> да");
+else
+Console.WriteLine($"{num}-> нет");
+
