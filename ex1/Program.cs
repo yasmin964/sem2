e872baddd6a91cca71a7fc692e369e05c6807997
yasya <kalgexavva1970@icloud.com>
2022-10-18 20:51:44 +0300

    add new file

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//напишите программу ,которая выводит случайное число из отрезка[10,99]
+// и выводит максимальное из 2
+int num=new Random().Next(10,100);
+int a1= num /10;
+int a2=num%10;
+int max=a1;
+if (max<a2)
+a2=max;
+Console.WriteLine($"Максимальное число в цифре {num} {max}");
+
+
