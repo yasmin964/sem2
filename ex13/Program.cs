9ab9f17509717937e91a4e0f353ef3efe31326aa
yasya <kalgexavva1970@icloud.com>
2022-10-18 21:53:54 +0300

    some info

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите программу,которая выводит 3 цифру заданного числа или сообщае,что третьего числа нет
+Console.WriteLine("введите число: ");
+int num=int.Parse(Console.Readline()!);
+int a1=num/100;
+int a2=(num/10)%10;
+int a3=num%10;
+if ((num/10)%10)
+Console.WriteLine($"{num}-> {a3}");
+else
+Console.WriteLine($"{num}-> третьей цифры нет");
+
